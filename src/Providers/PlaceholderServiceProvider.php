624374c37b0d2fce2ac9f27476e7fa3f9eb037d9
Pasu <pascal.jordans@sundsits.de>
2017-08-08 16:05:46 +0200

    test

---
+++ src/Providers/PlaceholderServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+namespace Placeholder\Providers;
+
+use Plenty\Plugin\serviceProvider;
+
+class PlaceholderServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+      
+    }
+}
+ ?>
